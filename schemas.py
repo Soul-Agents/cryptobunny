193430b9666b2e21edebd746011bd05ad2b590fa
Adam J. Zasada <ajgzasada@gmail.com>
2024-12-04 22:23:02 +0800

    database-fixes

--- schemas.py
+++ schemas.py
@@ -1,12 +1,21 @@
-from typing import TypedDict, Optional
+from typing import TypedDict, Optional, List, Dict
 from datetime import datetime
+
+
+class PublicMetrics(TypedDict):
+    retweet_count: int
+    reply_count: int
+    like_count: int
+    quote_count: int
 
 
 class BaseTweet(TypedDict):
     tweet_id: str
     text: str
     created_at: datetime
-    author_id: int
+    author_id: str
+    lang: Optional[str]
+    public_metrics: PublicMetrics
 
 
 class Tweet(BaseTweet):
@@ -15,23 +24,32 @@ class Tweet(BaseTweet):
     conversation_id: Optional[str]
     in_reply_to_user_id: Optional[str]
     in_reply_to_tweet_id: Optional[str]
-    like_count: int
-    reply_count: int
-    retweet_count: int
-    quote_count: int
+    public_metrics: PublicMetrics
+    replied_to: bool
+    replied_at: Optional[datetime]
 
 
-class WrittenAITweet(BaseTweet):
-    prompt: str
-    model: str
-    temperature: float
-    max_tokens: int
-    response_tweet_id: Optional[str]
+class WrittenAITweet(TypedDict):
+    tweet_id: str
+    edit_history_tweet_ids: List[str]
+    saved_at: datetime
+    text: str
+    public_metrics: Optional[PublicMetrics]
+    conversation_id: Optional[str]
+    in_reply_to_user_id: Optional[str]
+
+
+class WrittenAITweetReply(TypedDict):
+    tweet_id: str
+    reply: Dict[str, str]
+    public_metrics: Optional[PublicMetrics]
+    conversation_id: Optional[str]
+    in_reply_to_user_id: Optional[str]
 
 
 class ReplyToAITweet(TypedDict):
-    id: int
+    reply_id: str
     text: str
     created_at: datetime
     author_id: str
-    in_reply_to_tweet_id: int
+    in_reply_to_tweet_id: str
