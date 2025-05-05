1baab87655914c51ac4c7c8b902fb4fb846b4304
ankerx <sebastianoldak99@gmail.com>
2025-05-05 12:41:27 -0400

    fix packages in req

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app import create_app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
+    # This will only run in development
     app.run(debug=True, host='0.0.0.0', port=8000)
